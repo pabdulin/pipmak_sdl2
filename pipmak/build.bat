033292c0014f17c4915296f774153a534f9019ef
Petr Abdulin <pabdulin@gmail.com>
2023-12-20 22:33:34 +0700

    Switch to SDL2 libs and includes

--- pipmak/build.bat
+++ pipmak/build.bat
@@ -3,17 +3,21 @@ ECHO CWD=%cd%
 
 CALL "misc\ensureShell64.bat"
 
-SET LibsWin=Shell32.lib User32.lib Advapi32.lib Comdlg32.lib OpenGL32.Lib GlU32.Lib
 SET LibsSDL1=SDL.lib SDLmain.lib SDL_image.lib SDL_ttf.lib
-SET LibsSDL2=
+SET LibsSDL2=SDL2.lib SDL2main.lib SDL2_image.lib SDL2_ttf.lib
+SET LibsSDL=%LibsSDL2%
 SET LibsXiph=libogg.lib libvorbis.lib libvorbisfile.lib
+SET LibsWin=Shell32.lib User32.lib Advapi32.lib Comdlg32.lib OpenGL32.Lib GlU32.Lib
 SET LinkerPaths=/LIBPATH:..\lib
-SET LinkerFiles=lua503.lib %LibsSDL1% OpenAL32.lib %LibsXiph% %LibsWin%
+SET LinkerFiles=lua503.lib %LibsSDL% OpenAL32.lib %LibsXiph% %LibsWin%
 
 SET CompilerWarns=
 SET CompilerDefines=/D"DEBUG" /D"WIN32" /D"_USE_MATH_DEFINES"
+SET IncludeSDL1=/I..\include\SDL /I..\include\SDL_image /I..\include\SDL_ttf
+SET IncludeSDL2=/I..\include\SDL2 /I..\include\SDL2_image /I..\include\SDL2_ttf
+SET IncludeSDL=%IncludeSDL2%
+SET CompilerIncludePaths=/I..\include %IncludeSDL% /I..\include\AL /I..\include\lua503 /I..\include\physfs
 SET CompilerFiles="..\pipmak\code\*.c" "..\pipmak\code\physfs\*.c" "..\pipmak\code\gl3w\*.c"
-SET CompilerIncludePaths=/I..\include /I..\include\SDL /I..\include\SDL_image /I..\include\SDL_ttf /I..\include\AL /I..\include\lua503 /I..\include\physfs
 
 REM /Fe - Name EXE File, see: https://learn.microsoft.com/en-us/cpp/build/reference/fe-name-exe-file?view=msvc-170
 REM /MD, /MT, /LD - Use Run-Time Library, see: https://learn.microsoft.com/en-us/cpp/build/reference/md-mt-ld-use-run-time-library?view=msvc-170
