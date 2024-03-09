fd2ef3f78b4da5d0ea60e1a4eedabaad1f42e9e1
Petr Abdulin <pabdulin@gmail.com>
2024-03-09 18:18:12 +0700

    Reduce header, add copyright

--- pipmak/code/main.c
+++ pipmak/code/main.c
@@ -1,25 +1,5 @@
-/*
- 
- main.c, part of the Pipmak Game Engine
- Copyright (c) 2004-2011 Christian Walther
- 
- This program is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 2 of the License, or
- (at your option) any later version.
- 
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
- 
- You should have received a copy of the GNU General Public License
- along with this program; if not, write to the Free Software
- Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- 
- */
-
-/* $Id: main.c 228 2011-04-30 19:40:44Z cwalther $ */
+/* main.c, part of the Pipmak Game Engine
+Copyright (c) 2004-2011 Christian Walther, 2023-2024 Petr Abdulin */
 
 #ifndef _MSC_VER
 #include <unistd.h> /* chdir */
